35184ba4270e361c33019f5aa5b96e63e0b03d01
Kyrolos <pero.kero01@gmail.com>
2020-05-14 13:42:17 +0200

    Updates

---
+++ precache-manifest.19d47d06a4684ae8ae2afae73926bd8e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0575431bca86ef9eda98d6307f17eb02",
+    "url": "/ohi/index.html"
+  },
+  {
+    "revision": "92ac7d360e146e591c64",
+    "url": "/ohi/static/css/main.c602eb00.chunk.css"
+  },
+  {
+    "revision": "97921cb7761d8cc494c2",
+    "url": "/ohi/static/js/2.c1e30ca9.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/ohi/static/js/2.c1e30ca9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92ac7d360e146e591c64",
+    "url": "/ohi/static/js/main.8bcc7027.chunk.js"
+  },
+  {
+    "revision": "48e3dafe7c4c44372c4e",
+    "url": "/ohi/static/js/runtime-main.e1810383.js"
+  },
+  {
+    "revision": "e5193bd0bf6e368ddc2414c66a8dc090",
+    "url": "/ohi/static/media/Library.e5193bd0.jpg"
+  },
+  {
+    "revision": "c8f693fba4c89fddcd08610e3be827a6",
+    "url": "/ohi/static/media/logo.c8f693fb.png"
+  }
+]);
